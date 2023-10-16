cd0efb79213f2f549f9dfa4a98b7106b37183bf3
USER <USER@hzrhzln>
2023-10-16 18:02:10 +0800

    Welcome
    Java
    Programming!

---
+++ Multiple.java/src/Multiple.java
@@ -0,0 +1,9 @@
+
+public class Multiple {
+    public static void main(String[] args) {
+        // Print multiple lines of text
+        System.out.println("Welcome\n");
+        System.out.println("Java");
+        System.out.println("Programming!");
+    }
+}
